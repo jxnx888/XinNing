a30156d09f6a0de1b24d842d6361775537eec21f
Xin Ning <jxnx888@hotmail.com>
2018-05-30 22:52:51 +0800

    Mobile CSS and ZhuangBiShenQi

---
+++ XinNing/ZhuangXShenQi.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+
+namespace Application
+{
+
+    public partial class ZhuangXShenQi : System.Web.UI.Page
+    { 
+        public void Page_Load(object sender, EventArgs e)
+        {
+            img.Visible = true;
+            imgEmpty.Visible = false;
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            lblName.Text = name.Text;
+            lblDeparture.Text = departure.Text;
+            lblArrival.Text = arrival.Text;
+            lblDate.Text = date.Text;
+            lblName1.Text = name.Text;
+            lblDeparture1.Text = departure.Text;
+            lblArrival1.Text = arrival.Text;
+            lblDate1.Text = date.Text;
+            img.Visible = false;
+            imgEmpty.Visible = true;
+        }
+    }
+}
